c5b9529108ad33e8ce510f664f77e5dbc64caa2e
Akhilesh <71922190+seasighter@users.noreply.github.com>
2022-07-12 11:35:21 +0530

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -1,4 +1,4 @@
-# we have to find the sum of two list indices that gives t
+# we have to find the sum of two list indices that gives target
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range (len(nums)-1):
